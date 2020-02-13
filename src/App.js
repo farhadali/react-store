b2f7502fcf424a9269e2aa619d605f9fc54396c5
farhadali0507@gmail.com <farhadali0507@gmail.com>
2020-02-13 09:00:19 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { FaBeer } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello react</h1>
+     <h1>Hello react <FaBeer /></h1>
     </div>
   );
 }
